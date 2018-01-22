58a81560247e04498e8c64e0d2a748bee2e846e5
Dwang <coderdwang@outlook.com>
2018-01-22 22:08:36 +0800

    增加手动检测粘贴板内容变化

--- DoubleiTunes/Function/Account/Controller/DTAccountViewController.m
+++ DoubleiTunes/Function/Account/Controller/DTAccountViewController.m
@@ -9,6 +9,7 @@
 #import "DTAccountViewController.h"
 #import "DTAccountView.h"
 #import "DTCreateAccountViewController.h"
+#import <MJRefresh.h>
 
 @interface DTAccountViewController ()
 
@@ -32,6 +33,18 @@
     [accountView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.equalTo(self.baseView.mas_top);
         make.left.right.bottom.equalTo(self.view);
+    }];
+    weakOBJ(self);
+    accountView.mj_header = [MJRefreshNormalHeader headerWithRefreshingBlock:^{
+        [weak_self.accountView.accountModelArrM removeAllObjects];
+        weak_self.accountView.accountModelArrM = nil;
+        [weak_self.accountView reloadData];
+        [weak_self.progressHUD showAnimated:YES];
+        weak_self.progressHUD.label.text = @"数据检测中...";
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.25 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [weak_self.accountView.mj_header endRefreshing];
+            [weak_self dismissProgressHUD];
+        });
     }];
 }
 
